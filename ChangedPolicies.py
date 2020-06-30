985a6d8f3c2785303acc012b4e7afa27410ae3a3
yaelg <yaelg@checkpoint.com>
2020-06-30 17:17:43 +0300

    Initial commit

---
+++ ChangedPolicies.py
@@ -0,0 +1,320 @@
+import argparse
+import base64
+import datetime
+import json
+import os
+import time
+import os.path
+
+from os import path
+from cpapi import APIClient, APIClientArgs
+
+# Script running time in seconds
+DATETIME_NOW = datetime.datetime.now().replace(microsecond=0)
+DATETIME_NOW_SEC = int(round(time.mktime(DATETIME_NOW.timetuple())))
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATETIME_NOW_STR = datetime.datetime.fromtimestamp(DATETIME_NOW_SEC).strftime(DATETIME_FORMAT)
+log_file = None
+
+
+def populate_parser():
+    parser = argparse.ArgumentParser(
+        description="Changed Policies Tool find which policies were changed between revisions.")
+    parser.add_argument("-o", "--output-file", required=False, default="Changed_policies.json",
+                        help="The name of output file")
+    parser.add_argument("-u", "--username", required=False, default=os.getenv('MGMT_CLI_USER'),
+                        help="The management administrator's user name.\nEnvironment variable: MGMT_CLI_USER")
+    parser.add_argument("-p", "--password", required=False,
+                        help="The management administrator's password.\nEnvironment variable: MGMT_CLI_PASSWORD")
+    parser.add_argument("-m", "--management", required=False, default=os.getenv('MGMT_CLI_MANAGEMENT', "127.0.0.1"),
+                        help="The management server's IP address (In the case of a Multi-Domain Environment, "
+                             "use the IP address of the MDS domain).\nDefault: 127.0.0.1\nEnvironment variable: "
+                             "MGMT_CLI_MANAGEMENT")
+    parser.add_argument("--port", "--server-port", required=False, default=os.getenv('MGMT_CLI_PORT', 443),
+                        help="The port of the management server\nDefault: 443\nEnvironment variable: MGMT_CLI_PORT")
+    parser.add_argument("-d", "--domain", required=False, default=os.getenv('MGMT_CLI_DOMAIN'),
+                        help="The name, uid or IP-address of the management domain\n"
+                             "Environment variable: MGMT_CLI_DOMAIN")
+    parser.add_argument('--root', '-r', choices=['true', 'false'],
+                        help='\b{%(choices)s}\nLogin as root. When running on the management server, '
+                             'use this flag with value set to \'true\' to login as Super User administrator.',
+                        metavar=" \b\b")
+    parser.add_argument("-c", "--changes", required=False,
+                        help="\'show-changes\' output encoded in base 64. "
+                             "Use this flag for integration with Smart Task.")
+
+    return parser.parse_args()
+
+
+# Print message with time description
+def print_msg(msg):
+    global log_file
+    print("[{}] {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg))
+    log_file.write("[{}] {}\n".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg))
+
+
+# Exit if the API-call failed & print error message
+def exit_failure(error_msg, response):
+    if response.success is False:
+        print_msg(error_msg + " Error: {}".format(response.error_message))
+        exit(1)
+
+
+def login(user_args, client):
+    if user_args.root is not None and user_args.root.lower() == 'true':
+        if user_args.management == '127.0.0.1':
+            login_res = client.login_as_root(domain=user_args.domain)
+        else:
+            print_msg("Error: Command contains ambiguous parameters. "
+                      "Management server remote ip is unexpected when logging in as root.")
+            exit(1)
+    else:
+        login_res = client.login(user_args.username, user_args.password, domain=user_args.domain, read_only=True)
+
+    exit_failure("Failed to login.", login_res)
+
+
+def find_object_usage_in_policy(client, object_id, changed_policies):
+    where_used = client.api_call(command='where-used', payload={'uid': object_id})
+    if where_used.success is False and "Requested object [" + object_id + "] not found" in where_used.error_message:
+        print_msg("Object " + object_id + " was not found, might be not supported object.")
+        return
+    exit_failure("failed to find usages for " + object_id, where_used)
+
+    get_policy_from_usage(changed_policies, where_used.data['used-directly'].get('access-control-rules', []))
+    get_policy_from_usage(changed_policies, where_used.data['used-directly'].get('nat-rules', []))
+
+
+def get_policy_from_usage(changed_policies, usages):
+    for usage in usages:
+        if usage.get('package', "") != "":
+            changed_policies.append(usage.get('package').get('name'))
+
+
+def get_all_packages(client, package_map):
+    # if package_map is not None:
+    if len(package_map) != 0:
+        return package_map
+
+    show_packages_res = client.api_query("show-packages", details_level="full", container_key="packages")
+    exit_failure("failed to get packages", show_packages_res)
+
+    for package in show_packages_res.data:
+        package_name = package.get('name')
+
+        for access_layer in package.get('access-layers', []):
+            add_layer_to_map(access_layer.get('uid'), package_map, package_name)
+
+        show_nat_res = client.api_call("show-nat-rulebase", {'package': package_name})
+        exit_failure("failed to get NAT rulebase", show_nat_res)
+        add_layer_to_map(show_nat_res.data.get('uid'), package_map, package_name)
+
+        add_layer_to_map(package.get('https-inspection-layer', {}).get('uid'), package_map, package_name)
+
+    return package_map
+
+
+def add_layer_to_map(layer_id, map_layers_in_packages, package_name):
+    if layer_id is None:
+        return
+
+    if layer_id in map_layers_in_packages:
+        map_layers_in_packages[layer_id].append(package_name)
+    else:
+        map_layers_in_packages[layer_id] = [package_name]
+
+
+def get_package_from_map_by_layer_id(client, packages_map, layer_uid, object_type):
+    changed_policies = []
+    if packages_map.get(layer_uid) is not None:
+        return packages_map.get(layer_uid)
+
+    else:
+        parent_layer = None
+        inline_layers_in_package = set()
+        current_layer_id = layer_uid
+
+        rulebase_type = object_type.replace("rulebase", "layer")
+        if rulebase_type.endswith("rule"):
+            rulebase_type = object_type.replace("rule", "layer")
+
+        while True:  # not supporting shared layers
+
+            rulebase_res = client.api_call("show-" + rulebase_type, {"uid": current_layer_id})
+            exit_failure("failed to get rulabase " + current_layer_id, rulebase_res)
+            inline_layers_in_package.add(current_layer_id)
+
+            if rulebase_res.data.get('parent-layer') is None:
+                print_msg("layer " + current_layer_id + " is not in used in any package")
+                # avoid this loop next time
+                for layer in inline_layers_in_package:
+                    packages_map.update({layer: []})
+                return changed_policies
+
+            if packages_map.get(rulebase_res.data.get('parent-layer')) is not None:
+                parent_layer = rulebase_res.data.get('parent-layer')
+                break
+
+            current_layer_id = rulebase_res.data.get('parent-layer')
+
+        for layer in inline_layers_in_package:
+            if layer in packages_map:
+                packages_map[layer] = packages_map[layer] + packages_map[parent_layer]
+            else:
+                packages_map[layer] = packages_map[parent_layer]
+
+        changed_policies = packages_map.get(parent_layer)
+
+    return changed_policies
+
+
+def calculate_changes(client, changes, packages_map, old_object):
+    if len(changes) == 0:
+        return []
+
+    changed_policies = []
+    for change in changes:
+        if len(change) == 0:
+            continue
+        changed_type = change.get('type').lower()
+        # currently not supported
+        if 'threat' in changed_type or 'https' in changed_type:
+            print_msg(changed_type + " changes are not supported.")
+            continue
+
+        # check if package
+        if changed_type == 'package' or 'policy' in changed_type or 'policies' in changed_type:
+            if change.get('name').startswith("##"):
+                changed_policies.append(change.get('name')[2:])
+            else:
+                changed_policies.append(change.get('name'))
+            continue
+
+        # check if layer or rulebase
+        if 'layer' in changed_type or 'rulebase' in changed_type:
+            packages_map = get_all_packages(client, packages_map)
+            changed_policies = changed_policies + get_package_from_map_by_layer_id(
+                client, packages_map, change.get('uid'), changed_type)
+            continue
+
+        # check if section
+        if 'section' in changed_type:
+            continue
+
+        # check if rule
+        if 'rule' in changed_type:
+            packages_map = get_all_packages(client, packages_map)
+            # Nat rule
+            if change.get('package') is not None:
+                changed_policies = changed_policies + get_package_from_map_by_layer_id(
+                    client, packages_map, change.get('package'), changed_type)
+            # rest of the rules
+            elif change.get('layer') is not None:
+                changed_policies = changed_policies + get_package_from_map_by_layer_id(
+                    client, packages_map, change.get('layer'), changed_type)
+
+            continue
+
+        # the changed object is an object
+        if not old_object:
+            find_object_usage_in_policy(client, change.get('uid'), changed_policies)
+
+    return changed_policies
+
+
+def print_output_to_file(changed_policies_list, session_id, domain, file_name):
+    domain_name = "SMC User"
+    if domain is not None:
+        domain_name = domain
+    print_msg("Updating output file " + file_name)
+    if path.exists(file_name):
+        with open(file_name, "r") as jsonFile:
+            try:
+                data = json.load(jsonFile)
+            except ValueError:
+                print_msg("Error: Decoding JSON has failed")
+                exit(1)
+    else:
+        data = {}
+
+    if data.get(domain_name) is None:
+        data[domain_name] = {}
+
+    if data.get(domain_name).get(session_id) is None:
+        data[domain_name][session_id] = []
+
+    data[domain_name][session_id] = changed_policies_list
+
+    with open(file_name, 'w') as jsonFile:
+        json.dump(data, jsonFile, indent=4)
+
+    print_msg("The changed policies are:")
+    global log_file
+    log_file.write("{}\n".format(json.dumps({session_id: data[domain_name][session_id]}, indent=4)))
+    print(json.dumps({session_id: data[domain_name][session_id]}, indent=4))
+
+    return {session_id: data[domain_name][session_id]}
+
+
+def main():
+    user_args = populate_parser()
+    show_changes_payload = {"details-level": "full"}
+
+    client_args = APIClientArgs(server=user_args.management, port=user_args.port)
+
+    with APIClient(client_args) as client:
+        global log_file
+        log_file = open('logfile_' + str(DATETIME_NOW_SEC) + '.txt', 'w+')
+        print_msg('logfile_' + str(DATETIME_NOW_SEC) + '.txt file was created')
+        # The API client, would look for the server's certificate SHA1 fingerprint in a file.
+        # If the fingerprint is not found on the file, it will ask the user if he accepts the server's fingerprint.
+        # In case the user does not accept the fingerprint, exit the program.
+        if client.check_fingerprint() is False:
+            print_msg("Could not get the server's fingerprint - Check connectivity with the server.")
+            exit(1)
+
+        login(user_args, client)
+        changed_policies_list = []
+        diff = {}
+
+        print_msg("Collecting data from machine..")
+        if user_args.changes is None:
+            changes_res = client.api_call(command='show-changes', payload=show_changes_payload)
+            if changes_res.success is False:
+                print_msg(" Error: \'show-changes\' command failed")
+                exit(1)
+
+            diffs = changes_res.data['tasks'][0]['task-details'][0]['changes']
+            if len(diffs) == 0:
+                print_msg("No changes in the session.")
+                print_msg("Operation completed.")
+                exit(0)
+            diff = diffs[0]
+        else:
+            decode = base64.b64decode(user_args.changes)
+            diff = json.loads(decode)
+            pass
+        package_map = {}
+
+        changed_policies_list = changed_policies_list + calculate_changes(
+            client, diff.get('operations').get('added-objects'), package_map, False)
+        changed_policies_list = changed_policies_list + calculate_changes(
+            client, list(map(lambda diff_: diff_.get('new-object', {}),
+                             diff.get('operations').get('modified-objects'))), package_map, False)
+
+        changed_policies_list = changed_policies_list + calculate_changes(
+            client, list(map(lambda diff_: diff_.get('old-object', {}),
+                             diff.get('operations').get('modified-objects'))), package_map, True)
+        changed_policies_list = changed_policies_list + calculate_changes(
+            client, diff.get('operations').get('deleted-objects'), package_map, True)
+
+        changed_policies_list = list(set(changed_policies_list))
+        output = print_output_to_file(changed_policies_list, diff.get('session').get('session-uid'), user_args.domain,
+                                      user_args.output_file)
+
+        print_msg("Operation completed.")
+        return output
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,125 @@
 # ChangedPolicies
+Check Point ChangedPolicies tool allows the user to know which policies were affected by changes that were made in the last published session.
+This is useful to determine which policies should be installed.
+
+You can adjust the code according to your organization’s policy / needs.
+
+  - This tool can be executed on Management Server / Multi-Domain servers of version of R80.10 and up.
+
+## Instructions
+Clone the repository with this command:
+```git
+git clone https://github.com/CheckPointSW/ChangedPolicies
+``` 
+or by clicking the _‘Download ZIP’_ button. 
+
+Download and install the [Check Point API Python SDK](https://github.com/CheckPointSW/cp_mgmt_api_python_sdk) 
+repository, follow the instructions in the SDK repository.
+
+## Usage Syntax
+```python ChangedPolicies.py [-c changes] [-u username] [-p password] [-m management] [-d domain] [-o output-file] [--port port]```
+
+• [-c --changes] (Optional): The \'show-changes\' API command output encoded in base 64. 
+Use this flag for integration with Smart Task.
+For more details follow the instructions below in the ["Integration with Smart Task"](#Integration with Smart Task) section
+
+• [-d --domain] (Optional): The name or uid of the Security Management Server domain.  
+When running the command on a Multi domain server the default domain is the "MDS".
+
+• [-o --output-file] (Optional): The output file name. The location in which to save the resulting .Json file.   
+The default is the current directory with the name 'Changed_policies.json'. 
+ 
+• [--port] (Optional): The port of WebAPI server on Security Management Server.  
+Default value is 443.
+
+Use "-h" option in order to see the full list of options to configure the tool  
+
+## Examples
+*   Running the tool on a remote management server: 
+<br>```python ChangedPolicies.py  -m 172.23.78.160 -u James -p MySecretPassword!```
+<br>The tool runs on a remote management server with IP address 172.23.78.160.
+
+*   Running the tool on a Multi-Domain Server for a specific domain: 
+<br>```python ChangedPolicies.py  -d 172.23.78.152 -u James -p MySecretPassword!```
+
+*   Running the tool on a Security Management Server with specific output file name: 
+<br>```python ChangedPolicies.py  -o "json_file.json" -u James -p MySecretPassword!```
+
+
+## Output
+The tool generates a Json file with the changed policies for the last published session. 
+if you execute this tool multiple times it will update the file with the aggregated changes.
+ 
+Example of output:
+```Git
+
+{
+    "Global": {
+        "4631ed09-6663-4d0c-95e0-95e26ef2a927": [
+            "Standard"
+        ]
+    },
+    "my_domain": {
+        "02d744e0-eb92-40d6-bb59-186c522b35c1": [
+            "policy package 1",
+            "Standard"
+        ]
+    }
+}
+
+``` 
+
+## Integration with Smart Task
+This tool can be integrated with Smart Task which is supported from R80.40.
+
+### Instructions:
+* Install the Check Point API Python SDK, follow the instructions for
+ [SDK usage from a management machine](https://github.com/CheckPointSW/cp_mgmt_api_python_sdk#sdk-usage-from-a-management-machine).
+* Copy the script 'ChangedPolicies.py' to your Management machine.
+* Execute the command `api fingerprint -f json | jq -r '.[0] | .["fingerprint-sha1"]'` and save the output.
+* Create file named `fingerprints.txt` in `/var/tmp/`. The content should be a Json that the key is the machine IP 
+and the value is the fingerprint (the output of the command above).<br>
+For example: <br>
+     ```Git
+    {
+        "172.23.78.160": "6713548716C586ECDBF1A6693CB440071F1C89E6"
+    } 
+    ``` 
+* Create new script in Smart Console <i>'GATEWAYS & SERVERS' > 'Scripts' > 'Script Repository' > 'new' </i> 
+and call it 'Changed policies'. <br>
+The content should contain the export command as in the SDK instruction above and the execution of the script 
+using the '-c' / '--changes' flag with '$1' as value.<br>
+For example: 
+    ```Git
+    
+    export PYTHONPATH=$PYTHONPATH:/home/admin/cp_mgmt_api_python_sdk/
+    
+    python /var/tmp/ChangedPolicies.py -r true -c $1
+    
+    
+    ``` 
+* Create new Smart Task in Smart Console:
+<br>&emsp;- Go to <i>'MANAGE & SETTINGS' > 'Tasks' > 'new'</i> and call it 'Changed policies'.
+<br>&emsp;- Turn the task on.
+<br>&emsp;- Under 'Trigger and Action' choose 'After Publish' as trigger and 'Run Script' as the action 
+and select the script 'Changed policies' from the repository.
+<br>&emsp;- Under 'Advanced' set the 'Time out' to be 300.
+* Publish the changes.
+
+For more information about Smart Task go to [R80.40 Administration Guide](https://sc1.checkpoint.com/documents/R80.40/WebAdminGuides/EN/CP_R80.40_SecurityManagement_AdminGuide/Content/Topics-SECMG/SmartTasks.htm?Highlight=smarttask)... #TODO:  add link
+
+## limitations
+
+* The script support only the changes of last published session.
+* The tool doesn't support changes in Threat and HTTPS layers.
+* The tool doesn't support shared layers.
+* In case of changes in Global domain in Multi Domain machine, 
+the script should be executed from the Global domain (use the flag `--domain Global`).  
+
+
+## Development Environment
+The tool is developed using Python language version 2.7, version 3.7 and [Check Point API Python SDK](https://github.com/CheckPointSW/cp_mgmt_api_python_sdk).
+
+
+
+
